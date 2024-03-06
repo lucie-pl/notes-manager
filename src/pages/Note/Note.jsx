801e1f7878fcaa0f3da612ea3bb934f785cfd6d4
Lucie Plantier <lucie.plantier@live.fr>
2024-03-06 12:50:06 -0500

    Getting the noteId from the url and matching store
    Warning, need to transform string into number to be able to compare values

--- src/pages/Note/Note.jsx
+++ src/pages/Note/Note.jsx
@@ -1,5 +1,17 @@
+import { useParams } from 'react-router-dom';
 import s from './style.module.css';
+import { useSelector } from 'react-redux';
 
 export function Note(props) {
+  const { noteId } = useParams();
+  //To select key and value from params, we use useSearchParams() hook
+  //ex: [searchParams] = useSearchParams()
+  //ex: {searchParams.get('name')} => Toto
+
+  //Warning, below we compare a number (note.id) to a string (noteId)
+  //That's why we need to tranform the string in number
+  const note = useSelector((store) =>
+    store.NOTE.noteList.find((note) => note.id === parseFloat(noteId))
+  );
   return <>Note</>;
 }
