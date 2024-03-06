248d41ca5d3cb642c9caf18fd75757b2faaa7bf2
Lucie Plantier <lucie.plantier@live.fr>
2024-03-05 20:02:56 -0500

    Add useEffect to fetch our notes while launching
    Needed to create asynf function to call the API.
    Also adding dispatch hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from 'components/Header/Header';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { NoteAPI } from 'api/note-api';
+import { setNoteList } from 'store/note/note-slice';
 
 export function App() {
+  const dispatch = useDispatch();
+
+  async function fetchAllNotes() {
+    const noteList = await NoteAPI.fetchAll();
+    dispatch(setNoteList(noteList));
+  }
+
+  useEffect(() => {
+    fetchAllNotes();
+  }, []);
+
   return (
     <div className="container-fluid">
       <Header />
