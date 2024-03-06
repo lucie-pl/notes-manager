fdbc7240f5092718356ccf6c24619a23b238f584
Lucie Plantier <lucie.plantier@live.fr>
2024-03-05 20:20:21 -0500

    Add component to display note's list from store
    Adding useSelector hook to get data from the store
    Adding useNavigate hook to navigate to specific note on click
    Styling card

---
+++ src/containers/NoteList/NoteList.jsx
@@ -0,0 +1,27 @@
+import { useSelector } from 'react-redux';
+import { TextCard } from 'components/TextCard/TextCard';
+import s from './style.module.css';
+import { useNavigate } from 'react-router-dom';
+
+export function NoteList(props) {
+  const noteList = useSelector((store) => store.NOTE.noteList);
+  const navigate = useNavigate();
+
+  return (
+    <div className="row justify-content-center">
+      {noteList.map((note) => {
+        return (
+          <div className={s.card_container}>
+            <TextCard
+              title={note.title}
+              subtitle={note.subtitle}
+              content={note.content}
+              onClick={() => navigate('note/' + note.id)}
+              onClickTrash={() => alert('Click trash')}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
