8a824e5318e2abce458009621cbfa8b316578622
Lucie Plantier <lucie.plantier@live.fr>
2024-03-06 11:38:06 -0500

    Link form values received to back end store
    Add useDispatch hook to write in the store
    Add navigate hook to redirect to / after submitting form

--- src/pages/NoteCreate/NoteCreate.jsx
+++ src/pages/NoteCreate/NoteCreate.jsx
@@ -1,10 +1,25 @@
 import s from './style.module.css';
 import { NoteForm } from 'components/NoteForm/NoteForm';
+import { NoteAPI } from 'api/note-api';
+import { useDispatch } from 'react-redux';
+import { addNote } from 'store/note/note-slice';
+import { useNavigate } from 'react-router-dom';
 
 export function NoteCreate(props) {
+  const dispath = useDispatch();
+  const navigate = useNavigate();
+
+  async function createNote(formValues) {
+    const createdNote = await NoteAPI.create({
+      ...formValues,
+      created_at: new Date().toLocaleDateString(),
+    });
+    dispath(addNote(createdNote));
+    navigate('/');
+  }
   return (
     <>
-      <NoteForm title="Create a note" />
+      <NoteForm title="Create a note" onSubmit={createNote} />
     </>
   );
 }
