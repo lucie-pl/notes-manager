d64cdcab942ef488a90489cfb8ec90926d36f96f
Lucie Plantier <lucie.plantier@live.fr>
2024-03-06 11:43:47 -0500

    Add addNote action to push new note in the store

--- src/store/note/note-slice.js
+++ src/store/note/note-slice.js
@@ -9,9 +9,12 @@ export const noteSlice = createSlice({
     setNoteList: (currentSlice, action) => {
       currentSlice.noteList = action.payload;
     },
+    addNote: (currentSlice, action) => {
+      currentSlice.noteList.push(action.payload);
+    },
   },
 });
 
 export const noteReducer = noteSlice.reducer;
 
-export const { setNoteList } = noteSlice.actions;
+export const { setNoteList, addNote } = noteSlice.actions;
